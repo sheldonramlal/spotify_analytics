b13e0968c1313246e562f5c7362a1b238288c220
Sheldon <sheldonramlal99@gmail.com>
2026-04-01 21:40:49 -0400

    fixed padding on login screen

--- Frontend/src/components/LoginPage.tsx
+++ Frontend/src/components/LoginPage.tsx
@@ -5,7 +5,7 @@ const LoginPage = () => {
     return (
         <div className="flex items-center justify-center w-full h-screen text-white bg-black">
             
-            <div className="mx-auto max-w-7xl">
+            <div className="px-5 mx-auto max-w-7xl">
 
                 <div className="space-y-10 text-center ">
                     <h1 className="text-6xl font-semibold text-white "> See your Spotify Analytics</h1>
